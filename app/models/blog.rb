2e779015d4749e671c80df3126b14fc0b6f81061
ankion <ankion@gmail.com>
2014-08-27 16:25:59 +0800

    Modify fqdn allow blank.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,8 @@ class Blog < ActiveRecord::Base
   has_many :posts, :dependent => :destroy
   has_many :categories, :dependent => :destroy
   validates_presence_of :subdomain, :title
-  validates_uniqueness_of :subdomain, :fqdn
+  validates_uniqueness_of :subdomain
+  validates_uniqueness_of :fqdn, :allow_blank => true, :allow_nil => true
   validates_format_of :subdomain, :with => /\A[a-z][a-z0-9]{2,17}\z/i
   validates_exclusion_of :subdomain, :in => [
     "www",
