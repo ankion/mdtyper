c8e8c93fc17bffe0936f577bf70117919ce30305
ankion <ankion@gmail.com>
2014-09-11 09:28:49 +0800

    Category require name.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ActiveRecord::Base
   belongs_to :blog
   has_many :post_categoryships, :dependent => :destroy
   has_many :posts, :through => :post_categoryships
+  validates_presence_of :name
 
   def to_param
     "#{name}"
